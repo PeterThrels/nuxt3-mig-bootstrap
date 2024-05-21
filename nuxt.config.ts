858cb2b2418b36a3a2013ff46317160cd6af58fd
Peter Oduntan <peteroduntan@Peters-MacBook-Pro.local>
2024-05-21 08:02:38 +0100

    add compat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  vite: {
+    resolve: {
+      alias: {
+        vue: '@vue/compat'
+      }
+    },
+  },
   plugins: ["~/plugins/bootstrap-vue"],
 });
